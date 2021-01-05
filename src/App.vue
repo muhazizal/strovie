de0080ae4c17f457b7cb7be47065159d5b8da58f
muhazizal <muhazizal.26@gmail.com>
2021-01-05 22:45:55 +0700

    testing themoviedb api and axios config

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,25 @@
 </template>
 
 <script>
+import axios from "@/assets/global/axios-config.js";
+import API_ENDPOINT from "@/assets/global/api-endpoint.js";
+
 export default {
   name: "App",
 
   data: () => ({
-    //
+    popular: null,
   }),
+
+  mounted() {
+    axios
+      .get(`${API_ENDPOINT.GET_POPULAR}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
---
+++ src/assets/global/api-endpoint.js
@@ -0,0 +1,7 @@
+const API_KEY = "a9739b3415c360505a7d1db19829fe66";
+
+const API_ENDPOINT = {
+  GET_POPULAR: `popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
+
+export default API_ENDPOINT;
