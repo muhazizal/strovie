67222b6f08d45bf42385c8fffa0a462542f2903f
muhazizal <muhazizal.26@gmail.com>
2021-02-07 10:08:12 +0700

    fixing error on service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,8 @@ workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {
   ignoreURLParametersMatching: [/.*/],
 });
+
+workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate());
