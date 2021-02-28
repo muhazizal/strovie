81f3cb43bc0b9748215f49f7cd9f55b6aa5ee4a9
muhazizal <muhazizal.26@gmail.com>
2021-02-28 15:06:37 +0700

    added movie poster skeleton responsive design

--- src/components/Movie/MoviePosterSkeleton.vue
+++ src/components/Movie/MoviePosterSkeleton.vue
@@ -1,16 +1,43 @@
 <template>
-  <v-skeleton-loader
-    class="mx-auto"
-    min-width="250"
-    min-height="375"
-    max-width="350"
-    max-height="525"
-    type="image, image"
-  ></v-skeleton-loader>
+  <v-skeleton-loader class="mx-auto" width="100%" :max-width="maxWidth" :type="imageType"></v-skeleton-loader>
 </template>
 
 <script>
 export default {
   name: "MoviePosterSkeleton",
+
+  computed: {
+    // eslint-disable-next-line vue/return-in-computed-property
+    maxWidth() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return 250;
+        case "sm":
+          return 350;
+        case "md":
+          return 350;
+        case "lg":
+          return 350;
+        case "xl":
+          return 400;
+      }
+    },
+
+    // eslint-disable-next-line vue/return-in-computed-property
+    imageType() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return "image, image";
+        case "sm":
+          return "image, image";
+        case "md":
+          return "image, image";
+        case "lg":
+          return "image, image";
+        case "xl":
+          return "image, image, image";
+      }
+    },
+  },
 };
 </script>
