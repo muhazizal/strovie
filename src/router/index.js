21966500bbbefe267b8a9e6840d87c73a2323749
muhazizal <muhazizal.26@gmail.com>
2021-03-01 20:19:36 +0700

    fixing router behavior on savedPosition

--- src/router/index.js
+++ src/router/index.js
@@ -47,12 +47,9 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
-      return {
-        selector: savedPosition,
-        behavior: "smooth",
-      };
+      return savedPosition;
     } else {
       return {
         x: 0,
