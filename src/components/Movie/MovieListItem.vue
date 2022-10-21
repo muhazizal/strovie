c97a6c333009e30034c45e1ffb6064849379ab37
muhazizal <muhazizal.26@.com>
2022-10-21 10:00:58 +0700

    feat(master): fix movie vote rating

--- src/components/Movie/MovieListItem.vue
+++ src/components/Movie/MovieListItem.vue
@@ -1,62 +1,78 @@
 <template>
-  <v-col class="col-6 col-sm-4 col-lg-3 col-xl-2">
-    <router-link :to="`/movie/${movie.id}`" :key="$route.path" class="movie-link">
-      <movie-poster :movieDetail="movie">
-        <template v-slot:movie-item>
-          <v-card-title class="pb-1 px-3 font-weight-bold">{{ movie.title }}</v-card-title>
-          <v-card-text class="pb-0 px-3">
-            {{ movie.release_date }}
-          </v-card-text>
-          <v-card-text class="px-3 pb-6">
-            <v-row class="rating mx-0">
-              <v-rating :value="1" length="1" color="amber" background-color="amber" dense readonly size="20" />
-              <div class="rating ml-1">{{ movie.vote_average }} ({{ movie.vote_count }})</div>
-            </v-row>
-          </v-card-text>
-        </template>
-      </movie-poster>
-    </router-link>
-  </v-col>
+	<v-col class="col-6 col-sm-4 col-lg-3 col-xl-2">
+		<router-link
+			:to="`/movie/${movie.id}`"
+			:key="$route.path"
+			class="movie-link"
+		>
+			<movie-poster :movieDetail="movie">
+				<template v-slot:movie-item>
+					<v-card-title class="pb-1 px-3 font-weight-bold">{{
+						movie.title
+					}}</v-card-title>
+					<v-card-text class="pb-0 px-3">
+						{{ movie.release_date }}
+					</v-card-text>
+					<v-card-text class="px-3 pb-6">
+						<v-row class="rating mx-0">
+							<v-rating
+								:value="1"
+								length="1"
+								color="amber"
+								background-color="amber"
+								dense
+								readonly
+								size="20"
+							/>
+							<div class="rating ml-1">
+								{{ movie.vote_average.toFixed(2) }} ({{ movie.vote_count }})
+							</div>
+						</v-row>
+					</v-card-text>
+				</template>
+			</movie-poster>
+		</router-link>
+	</v-col>
 </template>
 <script>
 export default {
-  name: 'MovieListItem',
-  props: {
-    movie: Object,
-  },
-  components: {
-    MoviePoster: () => import('./MoviePoster.vue'),
-  },
+	name: 'MovieListItem',
+	props: {
+		movie: Object,
+	},
+	components: {
+		MoviePoster: () => import('./MoviePoster.vue'),
+	},
 }
 </script>
 <style lang="scss" scoped>
 .movie-link {
-  text-decoration: none;
+	text-decoration: none;
 }
 .v-card {
-  transition: box-shadow 0.5s;
-  &__title {
-    font-size: 0.875em;
-    line-height: 1.25rem;
-    word-break: break-word;
-    display: -webkit-box;
-    -webkit-line-clamp: 1;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-  }
-  &__text {
-    font-size: 0.75em;
-    font-weight: 500;
-  }
-  &:hover {
-    box-shadow: 0px 0px 15px 3px lightgrey !important;
-  }
-  .rating {
-    font-weight: 500;
-    padding-top: 1.5px;
-  }
-  .v-image {
-    min-height: 18.75rem;
-  }
+	transition: box-shadow 0.5s;
+	&__title {
+		font-size: 0.875em;
+		line-height: 1.25rem;
+		word-break: break-word;
+		display: -webkit-box;
+		-webkit-line-clamp: 1;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
+	&__text {
+		font-size: 0.75em;
+		font-weight: 500;
+	}
+	&:hover {
+		box-shadow: 0px 0px 15px 3px lightgrey !important;
+	}
+	.rating {
+		font-weight: 500;
+		padding-top: 1.5px;
+	}
+	.v-image {
+		min-height: 18.75rem;
+	}
 }
 </style>
--- src/components/Movie/MovieOverview.vue
+++ src/components/Movie/MovieOverview.vue
@@ -1,110 +1,123 @@
 <template>
-  <div class="overview">
-    <div class="overview-header">
-      <h3 class="header-title">{{ movieDetail.title }}</h3>
-      <p class="header-tagline">{{ movieDetail.tagline }}</p>
-      <p class="header-time mb-3">{{ movieDetail.release_date }} &bull; {{ convertedRuntime }}</p>
-      <v-row class="mx-0">
-        <v-rating
-          :value="movieDetail.vote_average / 2"
-          color="amber"
-          background-color="amber"
-          dense
-          half-increments
-          readonly
-          size="16"
-        />
-        <div class="header-rating ml-4">{{ movieDetail.vote_average }} ({{ movieDetail.vote_count }})</div>
-      </v-row>
-    </div>
-    <div class="overview-genres mt-6">
-      <h3 class="genres-title mb-1">Genres</h3>
-      <div class="genres-container d-flex flex-wrap">
-        <v-chip class="genres-item mr-3 mb-3" v-for="genre in movieDetail.genres" :key="genre.id" label outlined small>
-          <v-icon left small>mdi-play-circle</v-icon>
-          {{ genre.name }}
-        </v-chip>
-      </div>
-    </div>
-    <div class="overview-synopsis mt-1">
-      <h3 class="synopsis-title mb-1">Synopsis</h3>
-      <p class="synopsis-caption mb-5">{{ movieDetail.overview }}</p>
-    </div>
-    <div class="overview-actions d-flex flex-wrap mb-n3 mb-md-0">
-      <v-btn
-        class="mr-3 mb-3 mb-md-0"
-        rounded
-        outlined
-        tag="a"
-        :href="movieDetail.homepage"
-        target="_blank"
-        noreferer
-        :small="$vuetify.breakpoint.xs"
-      >
-        <v-icon left>mdi-web</v-icon>
-        Website
-      </v-btn>
-      <v-btn
-        class="mr-3 mb-3 mb-md-0"
-        rounded
-        outlined
-        tag="a"
-        :href="`https://www.imdb.com/title/${movieDetail.imdb_id}`"
-        :small="$vuetify.breakpoint.xs"
-        target="_blank"
-        noreferer
-      >
-        <v-icon left>mdi-star-circle</v-icon>
-        IMDB
-      </v-btn>
-      <movie-videos v-if="movieVideos.results" :movieVideos="movieVideos" />
-    </div>
-  </div>
+	<div class="overview">
+		<div class="overview-header">
+			<h3 class="header-title">{{ movieDetail.title }}</h3>
+			<p class="header-tagline">{{ movieDetail.tagline }}</p>
+			<p class="header-time mb-3">
+				{{ movieDetail.release_date }} &bull; {{ convertedRuntime }}
+			</p>
+			<v-row class="mx-0">
+				<v-rating
+					:value="movieDetail.vote_average / 2"
+					color="amber"
+					background-color="amber"
+					dense
+					half-increments
+					readonly
+					size="16"
+				/>
+				<div class="header-rating ml-4">
+					{{ movieDetail.vote_average.toFixed(2) }} ({{
+						movieDetail.vote_count
+					}})
+				</div>
+			</v-row>
+		</div>
+		<div class="overview-genres mt-6">
+			<h3 class="genres-title mb-1">Genres</h3>
+			<div class="genres-container d-flex flex-wrap">
+				<v-chip
+					class="genres-item mr-3 mb-3"
+					v-for="genre in movieDetail.genres"
+					:key="genre.id"
+					label
+					outlined
+					small
+				>
+					<v-icon left small>mdi-play-circle</v-icon>
+					{{ genre.name }}
+				</v-chip>
+			</div>
+		</div>
+		<div class="overview-synopsis mt-1">
+			<h3 class="synopsis-title mb-1">Synopsis</h3>
+			<p class="synopsis-caption mb-5">{{ movieDetail.overview }}</p>
+		</div>
+		<div class="overview-actions d-flex flex-wrap mb-n3 mb-md-0">
+			<v-btn
+				class="mr-3 mb-3 mb-md-0"
+				rounded
+				outlined
+				tag="a"
+				:href="movieDetail.homepage"
+				target="_blank"
+				noreferer
+				:small="$vuetify.breakpoint.xs"
+			>
+				<v-icon left>mdi-web</v-icon>
+				Website
+			</v-btn>
+			<v-btn
+				class="mr-3 mb-3 mb-md-0"
+				rounded
+				outlined
+				tag="a"
+				:href="`https://www.imdb.com/title/${movieDetail.imdb_id}`"
+				:small="$vuetify.breakpoint.xs"
+				target="_blank"
+				noreferer
+			>
+				<v-icon left>mdi-star-circle</v-icon>
+				IMDB
+			</v-btn>
+			<movie-videos v-if="movieVideos.results" :movieVideos="movieVideos" />
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  name: "MovieOverview",
-  props: {
-    movieDetail: Object,
-  },
-  components: {
-    MovieVideos: () => import("@/components/Movie/MovieVideos"),
-  },
-  computed: {
-    convertedRuntime() {
-      const hours = Math.floor(this.movieDetail.runtime / 60);
-      const minutes = this.movieDetail.runtime % 60;
-      return `${hours}h ${minutes}m`;
-    },
-    movieVideos() {
-      return this.$store.getters["movie/getMovieVideos"];
-    },
-  },
-  async created() {
-    const movieId = this.$route.params.id;
+	name: 'MovieOverview',
+	props: {
+		movieDetail: Object,
+	},
+	components: {
+		MovieVideos: () => import('@/components/Movie/MovieVideos'),
+	},
+	computed: {
+		convertedRuntime() {
+			const hours = Math.floor(this.movieDetail.runtime / 60)
+			const minutes = this.movieDetail.runtime % 60
+			return `${hours}h ${minutes}m`
+		},
+		movieVideos() {
+			return this.$store.getters['movie/getMovieVideos']
+		},
+	},
+	async created() {
+		const movieId = this.$route.params.id
 
-    if (movieId) {
-      await this.$store.dispatch("movie/movieVideos", movieId);
-    }
-  },
-};
+		if (movieId) {
+			await this.$store.dispatch('movie/movieVideos', movieId)
+		}
+	},
+}
 </script>
 <style lang="scss" scoped>
 .header {
-  &-title {
-    font-size: 1.5em;
-    word-break: break-word;
-  }
-  &-rating {
-    font-size: 0.813em;
-    padding-top: 0.25rem;
-  }
+	&-title {
+		font-size: 1.5em;
+		word-break: break-word;
+	}
+	&-rating {
+		font-size: 0.813em;
+		padding-top: 0.25rem;
+	}
 }
 .synopsis {
-  &-caption {
-    font-size: 0.875em;
-    line-height: 1.75em;
-    word-break: break-word;
-  }
+	&-caption {
+		font-size: 0.875em;
+		line-height: 1.75em;
+		word-break: break-word;
+	}
 }
 </style>
