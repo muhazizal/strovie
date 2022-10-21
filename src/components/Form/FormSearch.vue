4541665369618a70818bbde83a3c7fbf7d292847
muhazizal <muhazizal.26@.com>
2022-10-21 10:17:45 +0700

    feat(master): fix search toggle drawer

--- src/components/Form/FormSearch.vue
+++ src/components/Form/FormSearch.vue
@@ -1,68 +1,67 @@
 <template>
-  <form class="d-flex justify-center" :class="inDrawer ? 'inDrawer' : ''" @submit.prevent="searchMovies">
-    <v-theme-provider root>
-      <v-text-field
-        v-model="movie"
-        dense
-        outlined
-        clearable
-        name="movie"
-        type="search"
-        hide-details="auto"
-        placeholder="Search Movie"
-        prepend-inner-icon="mdi-magnify"
-        @keydown="handleKeydownSearchMovie"
-        @keydown.enter.prevent="searchMovies"
-      />
-    </v-theme-provider>
-  </form>
+	<form
+		class="d-flex justify-center"
+		:class="inDrawer ? 'inDrawer' : ''"
+		@submit.prevent="searchMovies"
+	>
+		<v-theme-provider root>
+			<v-text-field
+				v-model="movie"
+				dense
+				outlined
+				clearable
+				name="movie"
+				type="search"
+				hide-details="auto"
+				placeholder="Search Movie"
+				prepend-inner-icon="mdi-magnify"
+				@click:prepend-inner="searchMovies"
+				@keydown.enter.prevent="searchMovies"
+			/>
+		</v-theme-provider>
+	</form>
 </template>
 <script>
 export default {
-  name: 'FormSearch',
-  props: {
-    inDrawer: Boolean,
-    drawer: Boolean,
-  },
-  data() {
-    return {
-      movie: '',
-    }
-  },
-  methods: {
-    searchMovies() {
-      if (!this.movie) {
-        return
-      }
+	name: 'FormSearch',
+	props: {
+		inDrawer: Boolean,
+		drawer: Boolean,
+	},
+	data() {
+		return {
+			movie: '',
+		}
+	},
+	methods: {
+		searchMovies() {
+			if (!this.movie) {
+				return
+			}
 
-      this.$router
-        .push({
-          path: '/search',
-          query: {
-            title: this.movie,
-          },
-        })
-        .catch((error) => {
-          return error
-        })
+			this.$router
+				.push({
+					path: '/search',
+					query: {
+						title: this.movie,
+					},
+				})
+				.catch((error) => {
+					return error
+				})
 
-      this.$emit('toggleDrawer')
-    },
-    handleKeydownSearchMovie() {
-      setTimeout(() => {
-        this.searchMovies()
-      }, 1000)
-    },
-  },
+			this.$emit('toggleDrawer')
+		},
+	},
 }
 </script>
 <style lang="scss" scoped>
 form {
-  width: 100%;
-  max-width: 25rem;
+	width: 100%;
+	max-width: 25rem;
 }
 .inDrawer {
-  padding: 1rem 1rem 0 1rem;
-  max-width: 100% !important;
+	padding: 1rem 1rem 0 1rem;
+	max-width: 100% !important;
 }
 </style>
